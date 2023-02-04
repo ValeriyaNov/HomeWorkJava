2351116f235a96251662f86e8dff76fbdd5ee78b
Valeriya <wal.now@yandex.ru>
2023-02-04 21:25:24 +0300

    1

---
+++ ToSeminar4/Task1.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+
+public class  Task1 {
+    public static void main(String[] args){
+        //Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+        LinkedList<String> lst = new LinkedList<>();
+        lst.add("каждый");
+        lst.add("охотник");
+        lst.add("желает");
+        lst.add("знать");
+        lst.add("где");
+        lst.add("сидит");
+        lst.add("фазан");
+        System.out.println(lst);
+        System.out.print(Reverse(lst));
+
+
+}
+public static LinkedList<String> Reverse (LinkedList<String> arr){
+    for(int i = 0; i < (arr.size() / 2); i++){
+        String temp = arr.get(i);
+        arr.set(i, arr.get(arr.size() - i - 1));
+        arr.set(arr.size() - i - 1, temp);
+    }
+    
+    return arr;
+
+}
+}
